4199adf5dec47180533b2d85193403829e48127f
Christopher Wall <wallandall@gmail.com>
2018-06-02 15:43:49 +0200

    Added Contact and Groups Model

---
+++ db/migrate/20180602133758_create_contacts.rb
@@ -0,0 +1,15 @@
+class CreateContacts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :company
+      t.string :email
+      t.string :website
+      t.string :phone
+      t.string :address
+      t.references :group, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
