0b304636df981329a745de752614e6df22b2fad4
wallandall <wallandall@gmail.com>
2018-06-24 15:21:22 +0200

    Installed Pundit Gem and created policies for update and delete

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -26,9 +26,11 @@ class ContactsController < ApplicationController
   end
 
   def edit
+    authorize @contact
   end
 
   def update
+      authorize @contact
       if @contact.update(contact_params)
           flash[:success] = "Contact was successfully updated."
           redirect_to contacts_path(previous_query_string)
@@ -38,6 +40,7 @@ class ContactsController < ApplicationController
   end
 
   def destroy
+      authorize @contact
       @contact.destroy
       flash[:success] = "Contact was successfully deleted."
       redirect_to contacts_path
---
+++ app/policies/contact_policy.rb
@@ -0,0 +1,8 @@
+class ContactPolicy < ApplicationPolicy
+  def update?
+    user.id === record.user_id
+  end
+  def destroy?
+    update?
+  end
+end
